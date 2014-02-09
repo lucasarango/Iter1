52f82e331fce85c91caaa1945c32627751188bc0
unknown <jonathan.howe.yu@gmail.com>
2014-02-09 00:01:31 -0500

    Expand player class

--- Player.java
+++ Player.java
@@ -1,9 +1,15 @@
+import java.util.NoSuchElementException;
+import java.awt.Color;
+/**************************************************************************************
+ *  Player is a state object managed by GameMaster. It doesn't influence the game on
+ *  its own and is only accessible by GameMaster. 
+ **************************************************************************************/
 public class Player {
 	private int turnScore;
 	private String name;
 	private Color color;
 	private List<Developer> developers;
-	private List<Block> blocks;
+	private List<Block> blocksLeft;
 	private int actionTokens;
 	
 	public Player(String name, Color color, List<Developer> developers, List<Block> blocks) {
@@ -15,13 +21,42 @@ public class Player {
 		actionTokens = 3;
 	}
 	
+	public String getName() {
+		return name;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public List<Developer> getDevelopers() {
+		return developers;
+	}
+	
+	public List<Block> getBlocksLeft() {
+		return blocksLeft;
+	}
+	
+	public int getActionTokens() {
+		return actionTokens;
+	}
+	
 	public boolean useActionToken() {
-		if(actionToken > 0) {
-			actionToken--;
+		if(actionTokens > 0) {
+			actionTokens--;
 			return true;
 		}
 		else {
 			return false;
 		}
+	}
+	
+	public Developer getDeveloper(int coord_x, int coord_y) throws NoSuchElementException{
+		for(Developer d : developers) {
+			if(d.getXCoord() == coord_x && d.getYCoord() == coord_y) {
+				return d;
+			}
+		}
+		throw new NoSuchElementException();
 	}
 }
