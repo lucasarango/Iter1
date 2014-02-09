31330b32781ad7ed81bfb50c1ac0c1b91fd80fe2
unknown <jonathan.howe.yu@gmail.com>
2014-02-08 22:52:34 -0500

    Outline player class

--- Player.java
+++ Player.java
@@ -1,4 +1,27 @@
-
 public class Player {
-
+	private int turnScore;
+	private String name;
+	private Color color;
+	private List<Developer> developers;
+	private List<Block> blocks;
+	private int actionTokens;
+	
+	public Player(String name, Color color, List<Developer> developers, List<Block> blocks) {
+		turnScore = 0;
+		this.name = name;
+		this.color = color;
+		this.developers = developers;
+		this.blocks = blocks;
+		actionTokens = 3;
+	}
+	
+	public boolean useActionToken() {
+		if(actionToken > 0) {
+			actionToken--;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
