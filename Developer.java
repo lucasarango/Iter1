2aec63534c031df4844dcb27435d10f374fe2439
lucasarango <lucasarango@ufl.edu>
2014-02-08 23:31:25 -0500

    Dummy developer

---
+++ Developer.java
@@ -0,0 +1,15 @@
+public class Developer {
+	private int xCoord;
+	private int yCoord;
+
+	
+	public Developer() {
+
+	}
+	
+	public boolean move(int x, int y) {
+		this.xCoord= x;
+		this.yCoord= y;
+	}
+	
+}
