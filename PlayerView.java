846760610954da20831acca740dfe6a7e677c750
Sachit Mishra <smishra2@ufl.edu>
2014-02-09 19:37:46 -0500

    Separated views.

---
+++ PlayerView.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerView extends JFrame{
+	/**
+	 * This class represents the player info display. It creates a single 
+	 * visible window, split into a grid, 1 x 4, wherein each grid component
+	 * will display the info for a different player. 
+	 */
+	 
+	private JTextArea player1;
+	private JTextArea player2;
+	private JTextArea player3;
+	private JTextArea player4;
+	 
+	public PlayerView() {
+        try {
+            initialize();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        setVisible(true);
+	}
+	
+	/**
+	 * Initialize the contents of the this.
+	 */
+	private void initialize() throws Exception {
+		//First we set up the window. 
+        this.setTitle("Player Info: ");
+		this.setBounds(50, 50, 400, 200);
+        this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getContentPane().setLayout(new GridLayout(1, 4, 1 , 1));
+		
+		player1 = new JTextArea("P 1:\nDevs: \n1Blks: \n2Blks: \n3Blks: ");
+		player1.setEditable(false);
+		add(player1);
+		player2 = new JTextArea("P 2:\nDevs: \n1Blks: \n2Blks: \n3Blks: ");
+		player2.setEditable(false);
+		add(player2);
+		player3 = new JTextArea("P 3:\nDevs: \n1Blks: \n2Blks: \n3Blks: ");
+		player3.setEditable(false);
+		add(player3);
+		player4 = new JTextArea("P 4:\nDevs: \n1Blks: \n2Blks: \n3Blks: ");
+		player4.setEditable(false);
+		add(player4);
+	}
+}
+	
