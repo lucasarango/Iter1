75b849a209d4f558f4edb9437747efc107192886
Tyler Fink <tfink419@ufl.edu>
2014-02-09 23:14:31 -0500

    Git is acting weird

--- Board.java
+++ Board.java
@@ -1,21 +1,3 @@
-import java.util.ArrayList;
-<<<<<<< HEAD
-
-public class Board {
-
-	private Space[][] spaces;
-	private ArrayList<Block> threeBlocksLeft;
-	private int[] dimensions;
-
-	public Board() {
-		threeBlocksLeft = new ArrayList<Block>();
-
-		for (int i = 0; i < 56; i++) {
-			threeBlocksLeft.add(new ThreeBlock());
-		}
-
-		spaces = new Space[12][12];
-=======
 import java.util.*;
 
 public class Board{
@@ -33,31 +15,11 @@ public class Board{
 			threeBlocksLeft.add(new ThreeBlock());
 		}
 		
-		//The board needs a buffer zone on all sides because blocks can be placed on a board with just one tile on a space
 		spaces = new Space[14][14];
->>>>>>> board
 		dimensions = new int[2];
 		dimensions[0] = 12;
 		dimensions[1] = 12;
 
-<<<<<<< HEAD
-		for (int i = 0; i < dimensions[0]; i++) {
-			for (int j = 0; j < dimensions[1]; j++) {
-				spaces[i][j] = new Space();
-			}
-		}
-
-	}
-
-	public Board(int x, int y) {
-		threeBlocksLeft = new ArrayList<Block>();
-
-		for (int i = 0; i < 56; i++) {
-			threeBlocksLeft.add(new ThreeBlock());
-		}
-
-		spaces = new Space[x][y];
-=======
 		for(int i = 0; i < dimensions[0]+2; i++)
 		{
 			for(int j = 0; j < dimensions[1]+2; j++)
@@ -75,39 +37,22 @@ public class Board{
 		{
 			threeBlocksLeft.add(new ThreeBlock());
 		}
-		//The board needs a buffer zone on all sides because blocks can be placed on a board with just one tile on a space
-		spaces = new Space[x+2][y+2];
->>>>>>> board
+		
+		spaces = new Space[x][y];
 		dimensions = new int[2];
 		dimensions[0] = x;
 		dimensions[1] = y;
 
-<<<<<<< HEAD
-		for (int i = 0; i < dimensions[0]; i++) {
-			for (int j = 0; j < dimensions[1]; j++) {
-=======
-		for(int i = 0; i < dimensions[0]+2; i++)
+		for(int i = 0; i < dimensions[0]; i++)
 		{
-			for(int j = 0; j < dimensions[1]+2; j++)
+			for(int j = 0; j < dimensions[1]; j++)
 			{
->>>>>>> board
 				spaces[i][j] = new Space();
 			}
 		}
 
 	}
 
-<<<<<<< HEAD
-	public Block getThreeBlock() {
-		// initialize return value to false
-		Block ret = threeBlocksLeft.get(0);
-		;
-
-		// check for existing block
-		if (!threeBlocksLeft.isEmpty()) {
-			// select block
-			threeBlocksLeft.remove(0);
-=======
 	public Block getThreeBlock(){
 		//initialize return value to false
 		Block ret = null;
@@ -117,20 +62,11 @@ public class Board{
 		{
 			//select block
 			ret = threeBlocksLeft.remove(0);
->>>>>>> board
 		}
 
 		return ret;
 	}
 
-<<<<<<< HEAD
-	public boolean returnThreeBlock(Block b) {
-		// Put three block back if player doesn't want to place it
-		if (threeBlocksLeft.size() >= 56) {
-			threeBlocksLeft.add(b);
-			return true;
-		} else {
-=======
 	public boolean returnThreeBlock(Block b){
 		//Put three block back if player doesn't want to place it
 		if(threeBlocksLeft.size() >= 56)
@@ -140,17 +76,10 @@ public class Board{
 		}
 		else
 		{
->>>>>>> board
 			return false;
 		}
 	}
 
-<<<<<<< HEAD
-	public boolean placeBlock(Block b, Space s) {
-		// assumes block has been checked and exists
-
-		// initialize return value to false
-=======
 	public int getNumThreeBlocks()
 	{
 		return threeBlocksLeft.size();
@@ -160,24 +89,12 @@ public class Board{
 		//assumes block has been checked and exists
 
 		//initialize return value to false
->>>>>>> board
 		boolean ret = false;
 
 		Tile[][] tiles = b.getGrid();
 
 		int[] coord = findSpace(s);
 
-<<<<<<< HEAD
-		// iterate through grid and only place nonempty tiles
-		for (int i = 0; i < tiles.length; i++) {
-			for (int j = 0; j < tiles[i].length; j++) {
-				// check if empty part of grid
-				if (tiles[i][j] != null) {
-					// check for valid placement
-					if (checkPlacement(b, spaces[coord[0] - 1][coord[1] - 1])) {
-						spaces[coord[0] - 1][coord[1] - 1].placeBlock(b,
-								tiles[i][j]);
-=======
 		//iterate through grid and only place nonempty tiles
 		for(int i = 0; i < tiles.length; i++)
 		{
@@ -190,7 +107,6 @@ public class Board{
 					if(checkPlacement(b, spaces[coord[0]+i][coord[1]+j]))
 					{
 						spaces[coord[0]+i][coord[1]+j].placeBlock(b, tiles[i][j]);
->>>>>>> board
 					}
 				}
 			}
@@ -199,26 +115,6 @@ public class Board{
 		return ret;
 	}
 
-<<<<<<< HEAD
-	private boolean checkPlacement(Block b, Space spaces) {
-		// initialize return value
-
-		/*
-		 * Oops dont have to do error checking for(int i = 0; i < s.length; i++)
-		 * { if(s[i].getBlock() = ) }
-		 */
-
-		return true;
-
-	}
-
-	private int[] findSpace(Space s) {
-		// optimize this
-		int[] ret = { -1, -1 };
-		for (int i = 0; i < dimensions[0]; i++) {
-			for (int j = 0; j < dimensions[1]; j++) {
-				if (spaces[i][j].equals(s)) {
-=======
 	private boolean checkPlacement(Block b, Space s){
 		//initialize return value
 		boolean ret = false;
@@ -242,7 +138,6 @@ public class Board{
 			{
 				if(spaces[i][j].equals(s))
 				{
->>>>>>> board
 					ret[0] = i;
 					ret[1] = j;
 				}
@@ -252,12 +147,6 @@ public class Board{
 		return ret;
 	}
 
-<<<<<<< HEAD
-	public Space[][] getSpaces() {
-		return this.spaces;
-	}
-
-=======
 	public Space[][] getSpaces()
 	{
 		return this.spaces;
@@ -265,5 +154,4 @@ public class Board{
 
 
 
->>>>>>> board
 }
--- Space.java
+++ Space.java
@@ -1,57 +1,22 @@
-<<<<<<< HEAD
-public class Space {
-=======
 public class Space{
->>>>>>> board
 
 	int height;
 	Block block;
 	Tile tile;
-<<<<<<< HEAD
-
-	Space() {
-=======
 	
 	Space(){
->>>>>>> board
 		height = 0;
 		block = null;
 		tile = null;
 	}
 
-<<<<<<< HEAD
-	public void placeBlock(Block b, Tile t) {
-=======
 	public void placeBlock(Block b, Tile t)
 	{
->>>>>>> board
 		this.block = b;
 		height++;
 		this.tile = t;
 	}
 
-<<<<<<< HEAD
-	public Block getBlock() {
-		return this.block;
-	}
-
-	public Tile getTile() {
-		return this.tile;
-	}
-
-	public int getHeight() {
-		return this.height;
-	}
-
-	public boolean equals(Space s) {
-		if (this.height == s.getHeight() && this.tile.equals(s.getTile())
-				&& this.block.equals(s.getBlock())) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-=======
 	public Block getBlock()
 	{
 		return this.block;
@@ -66,5 +31,4 @@ public class Space{
 	{
 		return this.height;
 	}
->>>>>>> board
 }
