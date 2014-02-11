389eebf5ba4ed694347560f701d0d5543eb5790a
lucasarango <lucasarango@ufl.edu>
2014-02-10 19:50:20 -0500

    Fixed errors, still no BFS

--- Developer.java
+++ Developer.java
@@ -2,21 +2,20 @@ public class Developer {
 	private Space location;
 	private Player player;
 
-	
 	public Developer(Player p) {
 		this.player = p;
 	}
-	
+
 	public boolean move(Space s) {
-		this.location=s;
+		this.location = s;
 		return true;
 	}
-	
+
 	public Space getSpace() {
 		return location;
 	}
 
-	public void score(int score){
-		player.score(score);
+	public void score(int score) {
+		player.addToScore(score);
 	}
 }
